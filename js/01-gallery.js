a506b331ac4a4045a6a48848e8c9744affab537e
Alexander Andriyash <olexandr.andriyash@gmail.com>
2022-08-10 20:25:38 +0300

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const items = [];
+
+galleryItems.forEach(elem => {
+  const galleryItem = document.createElement('div');
+  galleryItem.classList = 'gallery__item';
+  const galleryLink = document.createElement('a');
+  galleryLink.classList = 'gallery__link';
+  galleryLink.href = 'elem.original';
+  const galleryImg = document.createElement('img');
+  galleryImg.classList = 'gallery__image';
+  galleryImg.src = elem.preview;
+  galleryImg.hasAttribute('data-source', elem.original);
+  galleryImg.alt = elem.description;
+
+  galleryItem.append(galleryLink);
+  galleryLink.append(galleryImg);
+  items.push(galleryItem);
+});
+
+gallery.append(...items);
+
+gallery.addEventListener('click', eve => {
+    eve.preventDefault();
+    if (eve.target.nodeName !== 'IMG') {
+  return
+}
+
+    const selectedImage = eve.target.getAttribute('data-source')
+
+    const instance = basicLightbox.create(`
+    <img src="${selectedImage}" width="800" height="600">
+`)
+
+    instance.show()
+    
+    gallery.addEventListener('keydown', eve => {
+    if (eve.key === 'Escape') {
+    instance.close()
+    }
+  })
+})
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const items = [];
+
+galleryItems.forEach(elem => {
+  const galleryLink = document.createElement('a');
+  galleryLink.className = 'gallery__item';
+  galleryLink.href = elem.original;
+  const galleryImg = document.createElement('img');
+  galleryImg.className = 'gallery__image';
+  galleryImg.src = elem.preview;
+  galleryImg.setAttribute('title', elem.description);
+  galleryImg.alt = elem.description;
+
+  galleryLink.append(galleryImg);
+  items.push(galleryLink);
+});
+
+gallery.append(...items);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250
+});
