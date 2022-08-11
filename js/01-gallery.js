50007646f194c817abb73e5b66edae02edf07c86
Alexander Andriyash <olexandr.andriyash@gmail.com>
2022-08-11 15:14:30 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -8,17 +8,20 @@ const items = [];
 galleryItems.forEach(element => {
   const galleryItem = document.createElement('div');
   galleryItem.className = 'gallery__item';
+
   const galleryLink = document.createElement('a');
   galleryLink.className = 'gallery__link';
   galleryLink.href = element.original;
+
   const galleryImage = document.createElement('img');
     galleryImage.className = 'gallery__image';
-    galleryImage.src = element.preview;;
+    galleryImage.src = element.preview;
     galleryImage.setAttribute('data-source', element.original);
     galleryImage.alt = element.description;
 
   galleryItem.append(galleryLink);
   galleryLink.append(galleryImage);
+  
   items.push(galleryItem);
 })
 
